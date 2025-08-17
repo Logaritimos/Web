808e16b1050f06d69a2778cb59bbc356201211b6
lcesar03 <luiz.camillo@sptech.school>
2025-08-17 00:38:43 -0300

    Comecei a adaptar o web-data-viz para o nosso projeto

---
+++ src/database/script-tabelas.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE logaritmos;
+
+USE logaritmos;
+
+CREATE TABLE empresa(
+idEmpresa INT PRIMARY KEY auto_increment,
+razaoSocial VARCHAR(200) NOT NULL,
+cnpj CHAR(14) NOT NULL,
+email VARCHAR(200) NOT NULL,
+senha VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE endereco(
+idEndereco INT PRIMARY KEY auto_increment,
+logradouro VARCHAR(200) NOT NULL,
+numero VARCHAR(15) NOT NULL,
+complemento VARCHAR(100),
+bairro VARCHAR(150) NOT NULL,
+cidade VARCHAR(150) NOT NULL,
+estado CHAR(2) NOT NULL,
+cep CHAR(8) NOT NULL,
+fkEmpresa INT NOT NULL,
+FOREIGN KEY(fkEmpresa) REFERENCES empresa(idEmpresa)
+)auto_increment = 100;
